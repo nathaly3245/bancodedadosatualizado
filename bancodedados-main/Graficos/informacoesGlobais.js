179cf849b27165dd4c0e37320d981d3969d96d0c
nathaly3245 <127852587+nathaly3245@users.noreply.github.com>
2024-10-30 09:07:48 -0300

    informacoesGlobais.js

--- bancodedados-main/Graficos/informacoesGlobais.js
+++ bancodedados-main/Graficos/informacoesGlobais.js
@@ -15,3 +15,5 @@ async function visualizarInformacoesGlobais() {
 }
 
 visualizarInformacoesGlobais();
+
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
